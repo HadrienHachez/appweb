6bbd6cf6458bc2840c8713f6cc2b4ab8df159e96
Hadrien Hachez <hadrienhachez@hotmail.com>
2019-01-05 21:06:30 +0100

    add first page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class HomeController extends AbstractController
+{
+    
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index():Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
